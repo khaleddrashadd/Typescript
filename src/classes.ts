a01b91f437fd3d3fc01171dcecdd9734c8665378
Khaled Rashad <khaledrashadd@outlook.com>
2024-03-27 01:44:55 +0200

    extending interfaces

--- src/classes.ts
+++ src/classes.ts
@@ -1,23 +1,67 @@
 //implement singleton pattern
 
- class A {
-  private static instance: A;
-  private constructor(private x: number, private y: number) {
-    this.x = x;
-    this.y = y;
-  }
-  public static getInstance(a: number, b: number) {
-    const i = new A(a, b);
-    if (!this.instance) {
-      this.instance = i;
-      return this.instance;
-    }
-    return this.instance;
-  }
-  public calc(): number {
-    return this.x + this.y;
-  }
+//  class A {
+//   private static instance: A;
+//   private constructor(private x: number, private y: number) {
+//     this.x = x;
+//     this.y = y;
+//   }
+//   public static getInstance(a: number, b: number) {
+//     const i = new A(a, b);
+//     if (!this.instance) {
+//       this.instance = i;
+//       return this.instance;
+//     }
+//     return this.instance;
+//   }
+//   public calc(): number {
+//     return this.x + this.y;
+//   }
+// }
+
+// console.log(A.getInstance(1, 2).calc());
+// console.log(A.getInstance(5, 6));
+
+//extending interfaces
+
+interface One {
+  x: number;
+  y: number;
 }
 
-console.log(A.getInstance(1, 2).calc());
-console.log(A.getInstance(5, 6));
+interface Two {
+  z: number;
+}
+
+//we can extend multiple interfaces unlike calsses
+interface Three extends One, Two {
+  add(): number;
+}
+//implementing interface three 
+
+// class A implements Three {
+//   constructor(public x: number, public y: number, public z: number) {}
+//   add(): number {
+//     return this.x + this.y + this.z;
+//   }
+// }
+
+//we can implement more than interface
+
+// class B implements One,Two {
+//   constructor(public x: number, public y: number, public z: number) {}
+
+// }
+
+
+//we can exrend and implement in the same class
+
+// class C implements One{
+//   constructor(public x: number, public y: number) {}
+// }
+
+// class D extends C implements Two {
+//   constructor(public x: number, public y: number, public z: number) {
+//     super(x, y);
+//   }
+// }
